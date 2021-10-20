439bf3c28e689c75e13da2e26e17a31364c823a7
ohidurgclan <ohidur.ru.2019@gmail.com>
2021-10-20 21:26:41 +0600

    last

--- src/components/HomeServices/HomeServices.js
+++ src/components/HomeServices/HomeServices.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import './HomeServices.css';
 
 const HomeServices = (props) => {
-    const { key, name, star, details, img } = props.service;
+    const { key, name, details, img } = props.service;
     return (
         <>
             <Col lg={6} md={6} sm={12}>
@@ -12,7 +12,6 @@ const HomeServices = (props) => {
                     <img className="img img-fluid card-img" src={img} alt="" />
                     <div className="service-info">
                         <h5 className="fw-bold">{name}</h5>
-                        <h6 className="fw-bold"><span>{star}</span></h6>
                         <div>
                             <p>{details.slice(0, 150)}</p>
                         </div>
